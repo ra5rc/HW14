09e727248bf7027d8312575789b50942147fdb6e
Rhea Agarwal <107501795+ra5rc@users.noreply.github.com>
2022-07-11 12:57:45 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='package14',
+      version='0.1',
+      description='A pointless demonstration package',
+      url='https://github.com/Humairabh/DS5100_package',
+      author='Humaira & Rhea',
+      author_email='hbh4bv@virginia.com',
+      license='MIT',
+      packages=['package'])

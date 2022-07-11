66ea782da9931b2a4e836e05397a8e6861f46e35
Rhea Agarwal <107501795+ra5rc@users.noreply.github.com>
2022-07-11 12:58:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='package14',
       version='0.1',
       description='A pointless demonstration package',
-      url='https://github.com/Humairabh/DS5100_package',
-      author='Humaira & Rhea',
-      author_email='hbh4bv@virginia.com',
+      url='https://github.com/ra5rc/HW14',
+      author='Rhea Agarwal',
+      author_email='ra5rc@virginia.com',
       license='MIT',
       packages=['package'])
